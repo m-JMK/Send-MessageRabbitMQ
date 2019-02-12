c05b2fa66ab05684a2ec1d040e4345872c6c049a
m-JMK <47538808+m-JMK@users.noreply.github.com>
2019-02-12 14:58:16 +0100

    Add files via upload

---
+++ Send-MessageRabbitMQ.ps1
@@ -0,0 +1,33 @@
+﻿#Sources
+#http://ramblingcookiemonster.github.io/RabbitMQ-Intro/
+<#
+$RabbitMQCred = Get-Credential -Message "Account for Rabbit MQ Server" -UserName "Collab_Admin"
+$RabbitMQCred | Select Username,@{Name="Password";Expression = {$_.password | ConvertFrom-SecureString}} | ConvertTo-Json | Out-File .\testcredentials.json #>
+
+#Define a default RabbitMq server and get a credential to use
+Set-RabbitMqConfig -ComputerName 'de0-vsiaas-1223.eu.airbus.corp'
+
+$in = Get-Content -Path '.\testcredentials.json' | Out-String | ConvertFrom-Json
+$login = $in.Username
+$password = ConvertTo-SecureString $in.Password
+$RabbitMQCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList @($login,$password)
+
+#Set some common parameters we will always use:
+$Params = @{
+    Credential = $RabbitMQCred
+}
+
+#Assumes an exchange and bound queue set up per RabbitMqTools example:
+$ExchangeName = "migration" 
+$QueueName = "Mig_CM"
+
+$object = [pscustomobject]@{
+    Some='Random CM message'
+    Data = $(Get-Date)
+}
+
+#Open a new PowerShell Window import PSRabbitMq, and send a persistent message
+Send-RabbitMqMessage -Exchange $ExchangeName -Key $QueueName -InputObject "Test from json cred" -vhost /collab @Params
+
+#Start Listener Rabbit MQ
+#Start-RabbitMqListener @params -Exchange "migration" -QueueName "Mig_CM" -vhost /collab
